fe8fed9a5fe4a4e8ced4b2763fbb5cea058bfdbc
CallumT45 <callumterr45@gmail.com>
2020-02-07 23:42:02 +0000

    Added Hangman

--- Discord_bot.py
+++ Discord_bot.py
@@ -80,7 +80,86 @@ async def deadline():
             print(e)
             await asyncio.sleep(20000)
 
-class game():
+class Hangman():
+    def __init__(self, channel):
+        self.words =  """grape catapult dog baboon elephant giraffe apple coconut monkey rubik mice mouse pineapple android apple house fence python grail zebra protein terrain llama fire policeman zebra lion fluffy
+        America Balloon Biscuit Blanket Chicken Chimney Country Cupcake Curtain Diamond Eyebrow Fireman Florida Germany Harpoon Husband Morning Octopus Popcorn Printer Sandbox Skyline Spinach
+        Backpack Basement Building Campfire Complete Elephant Exercise Hospital Internet Jalapeno Mosquito Sandwich Scissors Seahorse Skeleton Snowball Sunshade Treasure
+        Blueberry Breakfast Bubblegum Cellphone Dandelion Hairbrush Hamburger Horsewhip Jellyfish Landscape Nightmare Pensioner Rectangle Snowboard Spaceship Spongebob Swordfish Telephone Telescope
+        Bellpepper Broomstick Commercial Flashlight Lighthouse Lightsaber Microphone Photograph Skyscraper Strawberry Sunglasses Toothbrush Toothpaste
+        """.lower().split()
+        self.channel = channel
+        self.lives = 5
+        self.word_to_guess = random.choice(self.words)
+        self.split_wtg = [char for char in self.word_to_guess]
+        self.cg= ["_"]*len(self.split_wtg)#prints a list the length of the hidden word
+        self.letters_left="a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+
+    def game(self, guess):
+        """iterates through the split word and if the letter guess is at that element then place that letter in the 
+        current guess list, if not then increment count. After the for loop concludes if no matches were found
+        then decrement number of lives"""
+        count = 0
+        for i in range(len(self.split_wtg)):
+            if guess == self.split_wtg[i]:
+               self.cg[i]= guess
+            else:
+               count += 1
+        if count == len(self.split_wtg):
+            self.lives -= 1
+
+    def string_format(self, letters_list):
+        text = ''
+        for i in letters_list:
+            text += i + ' | ' 
+        return text
+    def string_format2(self, letters_list):
+        text = ''
+        for i in letters_list:
+            if i:
+                text += i
+            else:
+                text += '_'
+            # text += '.'+ i 
+        return text
+
+    async def maingame(self):
+        def letter_check(m):
+            return (m.content in self.letters_left or m.content.lower() == "stop") and m.channel == self.channel
+        await self.channel.send(f"Word has {len(self.cg)} letters")
+        while self.cg!=self.split_wtg:#While guessed word is not equal to the hidden word 
+            try:
+                await self.channel.send("Choose your letter")
+
+                try:
+                    guess = await client.wait_for('message', timeout=30.0, check=letter_check)
+
+                except:
+                    await self.channel.send("Timed Out!")
+                    break
+
+                self.letters_left.remove(guess.content)
+                self.game(guess.content)
+                letters_string = self.string_format(self.letters_left)
+                embed = discord.Embed(title="Hangman", color=0x00ff00)
+                embed.add_field(name="Letters Left", value=letters_string, inline=False)
+                embed.add_field(name="Lives", value=self.lives, inline=False)
+                embed.add_field(name="Current Guess", value=self.string_format2(self.cg), inline=False)
+
+                await self.channel.send(embed=embed)
+                # await self.channel.send(f"Letters Left\n{self.letters_left}\n\nYou have {self.lives} lives left\n\n{self.cg}")
+                if self.lives == 0:
+                    await self.channel.send(f"Word was {self.word_to_guess}")
+                    break#break the inside game loop, prompts the user to play again
+            except Exception as e:
+                # print(e)
+                continue
+        if self.cg == self.split_wtg:
+            await self.channel.send("Congratulations!")
+        else:
+            await self.channel.send("Better Luck Next Time!")
+
+class TicTacToe():
 
     def __init__(self,player_letter,channel):
         self.channel = channel
@@ -241,7 +320,9 @@ async def on_message(message):
         """
         await channel.send(output)
 
-
+    elif message.content == '$hangman':
+        hm = Hangman(channel)
+        await hm.maingame()
 
     elif message.content == '$tictactoe': 
         await channel.send('Do you want to be X or O?')
@@ -251,7 +332,7 @@ async def on_message(message):
         try:
             msg = await client.wait_for('message', timeout=30.0, check=check)
             letter = msg.content.upper()
-            tttGame = game(letter,channel)
+            tttGame = TicTacToe(letter,channel)
             await tttGame.drawBoard()
             await tttGame.mainGame()
         except:
@@ -319,15 +400,14 @@ async def on_message(message):
 @client.event
 async def on_reaction_add(reaction, user):
     """If reaction matches and there is text in the message then draw the text on the image, return the image"""
-    print("\u2753")
-    if reaction.emoji == ':question:' and reaction.message.content:
+    if reaction.emoji == "\u2753" and reaction.message.content:
         img = Image.open(r'files\Google_web_search.png')
         draw = ImageDraw.Draw(img)
 
         font = ImageFont.truetype(r"files\OpenSans-Regular.ttf", 18)
         # draw the message on the background
             
-        draw.text((380, 328), reaction.message.content, fill='rgb(0, 0, 0)', font=font)
+        draw.text((380, 328), reaction.message.content[:60], fill='rgb(0, 0, 0)', font=font)
         buff = io.BytesIO()
         img.save(buff, format ='PNG')
         byteImage = buff.getvalue()
